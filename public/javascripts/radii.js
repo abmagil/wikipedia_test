1ded20bdf672ba2f246ea9ec97c9f5509979142c
Aaron Magil <amagil@camsys.com>
2014-07-20 17:39:15 -0400

    pushes data from server to client. cannot really consume yet

--- public/javascripts/radii.js
+++ public/javascripts/radii.js
@@ -1,14 +1,26 @@
+
+var socket = io();
 var bbox, radii, svg, target;
      
-svg = d3.select('svg');
+$("body").on("click", function() {
+    socket.emit("need data");
+})
+socket.on("new data", function(r) {
+    console.log(r);
+    radii.push(r);
+});
+
+svg = d3.select("body").append('svg')
+        .attr("height", 600)
+        .attr("width", 600);
 bbox = svg[0][0].getBoundingClientRect();
  
-radii = [12, 24, 32, 48, 60, 80, 120, 210];
- 
+ // Center the circles in the bounding box
 target = svg.append('g')
     .attr('transform', "translate(" + (bbox.width / 2) + "," + (bbox.height / 2) + ")");
- 
-target.selectAll('circle')
-    .data(radii)
-  .enter().append('circle')
-    .attr('r', function(d) {console.log(d);return d;});
+
+
+target.selectAll('circle');
+//     .data(radii)
+//   .enter().append('circle')
+//     .attr('r', function(d) {return d;});
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var http = require('http').Server(app);
 var routes = require('./routes');
 var path = require('path');
 var io = require('socket.io')(http);
+var data = [12, 24, 32, 48, 60, 80, 120, 210 ];
+var requests = 0;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -13,17 +15,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 if ('dev' == app.get('env')) {
   app.use(express.errorHandler());
 }
-
 app.get('/', routes.index);
-
-
 app.set('port', 3001);
 
 io.on('connection', function(socket){
   console.log('user connected');
-  socket.on('chat message', function(msg){
-    console.log(msg);
-    io.emit('chat message', msg);
+  socket.on('need data', function(msg){
+    requests++;
+    io.emit('new data', data[requests % data.length]);
   });
   socket.on('disconnect', function(){
     console.log('user disconnected');
