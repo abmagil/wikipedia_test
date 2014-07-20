068aa20c36f3157cae98848f7de435e232c1b56a
Aaron Magil <amagil@camsys.com>
2014-07-20 14:01:28 -0400

    Initial Commit.  Push circles to the screen

---
+++ README.md
@@ -0,0 +1,6 @@
+Six Degrees of Wikipedia
+===========
+
+Wikipedia holds all the information in the history of ever.  Hypermedia connects all the pages.  But how is it all connected?
+
+This project takes in a wikipedia page, find all the pages that link to it, all the pages that link to those, etc., then displays it all out to the user so we can see just how interesting the world really is.
---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var routes = require('./routes');
+var path = require('path');
+var io = require('socket.io')(http);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('dev' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+
+app.set('port', 3001);
+
+io.on('connection', function(socket){
+  console.log('user connected');
+  socket.on('chat message', function(msg){
+    console.log(msg);
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
