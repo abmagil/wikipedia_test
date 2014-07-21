9a2570efc3d48b675798850bb027288c6fc6027c
Aaron Magil <amagil@camsys.com>
2014-07-21 19:44:52 -0400

    Can draw on screen when you push a number

--- public/javascripts/radii.js
+++ public/javascripts/radii.js
@@ -1,23 +1,35 @@
 
 var socket = io();
-var bbox, radii = [], svg, target;
-     
-$("body").on("click", function() {
-    socket.emit("need data");
-})
-socket.on("new data", function(r) {
-    radii.push(r);
-});
-
+var bbox, radii, svg, target;
+              
+// Create svg container     
 svg = d3.select("body").append('svg')
         .attr("height", 600)
         .attr("width", 600);
 bbox = svg[0][0].getBoundingClientRect();
- 
+// Path generator
+var circle = d3.svg.arc()
+              .innerRadius(function(d) {console.log(d);return d})
+              .outerRadius(bbox.width / 2)
+              .startAngle(0)
+              .endAngle(Math.PI);
  // Center the circles in the bounding box
 var target = svg.append('g')
     .attr('transform', "translate(" + (bbox.width / 2) + "," + (bbox.height / 2) + ")");
 
-var arc = d3.svg.arc()
-            .innerRadius(0)
-            .outerRadius(bbox.width / 2);
+
+$("body").on("click", function() {
+    socket.emit("need data");
+})
+socket.on("new data", function(r) {
+    radii = r;
+    console.log(r);
+    target.append("path")
+      .attr("d", circle(radii))
+      .attr("stroke", "blue")
+      .attr("stroke-width", 2)
+      .attr("fill", "none");
+});
+
+ 
+
