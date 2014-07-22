33ac2a8792e115405714a1ffd5f540bc606f2ba0
Aaron Magil <amagil@camsys.com>
2014-07-21 21:33:55 -0400

    TEMP commit.  Not working.  Can't get API JSON grab to succeed

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,5 @@
  */
 
 exports.index = function(req, res){
-	// HTTP MEDIA WIKI API // http://en.wikipedia.org/w/api.php?action=query&list=backlinks&bltitle=JSON&bllimit=300&format=json&blfilterredir=nonredirects&blnamespace=0&bldir=descending&blcontinue=0|JSON|42888724
 	res.render('index');
 };
